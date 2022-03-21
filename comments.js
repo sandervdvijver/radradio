2d1f51153b83bddfc5f594a8c974b7758530defa
Frederik De Bleser <frederik@debleser.be>
2022-03-21 20:36:48 +0100

    Integrate comments.

---
+++ comments.js
@@ -0,0 +1,47 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/9.6.9/firebase-firestore-lite.js";
+import { formatDistanceToNow } from "https://cdn.skypack.dev/date-fns";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACgBKFy7SZI6IW_ARvCwIEhYkMmb8UP58",
+  authDomain: "radradio-11329.firebaseapp.com",
+  projectId: "radradio-11329",
+  storageBucket: "radradio-11329.appspot.com",
+  messagingSenderId: "50631293869",
+  appId: "1:50631293869:web:4b40fc9d65c69aa6777f24",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+function formatDate(ts) {
+  const date = ts.toDate();
+  return formatDistanceToNow(date) + " ago";
+}
+
+// Get a list of cities from your database
+async function getComments(db) {
+  const commentsCollection = collection(db, "comments");
+  const commentsSnapshot = await getDocs(commentsCollection);
+  const comments = commentsSnapshot.docs.map((doc) => doc.data());
+  return comments;
+}
+
+async function loadComments() {
+  const comments = await getComments(db);
+  const $comments = document.querySelector(".comments");
+  for (const comment of comments) {
+    $comments.innerHTML += `
+      <div class="comment">
+      <div class="comment-date">${formatDate(comment.timestamp)}</div>
+        <div class="comment-body">${comment.body}</div>
+      </div>
+    `;
+  }
+}
+
+loadComments();
